bfc22b0748ddb8ccdaf0ac2d8679279d25f3e1d3
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-05-06 22:15:02 -0400

    add Hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import styles from "./App.module.css";
 import { About } from "./components/About/About";
 // import { Contact } from "./components/Contact/Contact";
 // import { Experience } from "./components/Experience/Experience";
-// import { Hero } from "./components/Hero/Hero";
+import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 // import { Projects } from "./components/Projects/Projects";
 
 function App() {
   return (
     <div className={styles.App}>
-      {<Navbar />
-      /* <Hero /> */}
+      <Navbar />
+      <Hero />
       <About />
       {/* <Experience />
       <Projects />
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Anderson Asprilla</h1>
+        <p className={styles.description}>
+        A two-time Fixed Operations Salesperson of the Year (2022, 2023) at an Automotive Dealer company, I've enhanced operational efficiency and customer satisfaction through innovative technology. My custom-developed app revolutionized our sales process by seamlessly integrating customer data and special order tracking, significantly boosting customer satisfaction.
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/stars.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
