5b27dd56682845bac5360af7baaa45431572fb1e
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-05-07 06:20:27 -0400

    add experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import styles from "./App.module.css";
 import { About } from "./components/About/About";
 // import { Contact } from "./components/Contact/Contact";
-// import { Experience } from "./components/Experience/Experience";
+import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 // import { Projects } from "./components/Projects/Projects";
@@ -12,9 +12,9 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-      {/* <Experience />
-      <Projects />
-      <Contact /> */}
+       <Experience />
+      {/* <Projects />
+      <Contact />  */}
     </div>
   );
 }
