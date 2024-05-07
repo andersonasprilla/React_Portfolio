5a68eeb54b5f13df6ccb51d99311de45eabf826e
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-05-06 21:54:41 -0400

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { About } from "./components/About/About";
 // import { Contact } from "./components/Contact/Contact";
 // import { Experience } from "./components/Experience/Experience";
 // import { Hero } from "./components/Hero/Hero";
-// import { Navbar } from "./components/Navbar/Navbar";
+import { Navbar } from "./components/Navbar/Navbar";
 // import { Projects } from "./components/Projects/Projects";
 
 function App() {
   return (
     <div className={styles.App}>
-      {/* <Navbar />
-      <Hero /> */}
+      {<Navbar />
+      /* <Hero /> */}
       <About />
       {/* <Experience />
       <Projects />
