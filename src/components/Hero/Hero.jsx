a6ec3f29bf05e80df0c942da96efd3086775d730
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-06-03 14:48:03 -0400

    add resume

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,7 +11,7 @@ export const Hero = () => {
         <p className={styles.description}>
         The creator of <a href="https://www.dsxpert.com" >dsxpert.com</a> and a two-time consecutive Fixed Operations Salesperson of the Year award at an Automotive Dealer company, I have driven significant enhancements in operational efficiency and customer satisfaction through innovative technology. My <a href="https://www.dsxpert.com">custom-developed app</a> has revolutionized our sales process by seamlessly integrating customer data and special order tracking, thereby significantly boosting customer satisfaction.
         </p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+        <a href="https://app.enhancv.com/share/d0f3980f/?utm_medium=growth&utm_campaign=share-resume&utm_source=dynamic" className={styles.contactBtn}>
           Resume
         </a>
       </div>
